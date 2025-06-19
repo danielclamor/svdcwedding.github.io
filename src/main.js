de5fa5938d62825bb0fdbcd44f4d451fb7924dd3
Daniel Clamor <danielclamor@gmail.com>
2025-06-18 18:58:37 -0600

    add style codes

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1 class="--font-pprader text-3xl font-bold underline">
-      Hello World!
-    </h1>
-  </div>
+<div class="flex justify-center"> 
+  <h1 class="text-3xl">
+    Hello World!
+  </h1>
+</div>
 `
 
 setupCounter(document.querySelector('#counter'))
