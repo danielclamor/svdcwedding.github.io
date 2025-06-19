cf721d57fef53d238ee51c3005b0e953b1da7f6d
Daniel Clamor <danielclamor@gmail.com>
2025-06-18 21:35:43 -0600

    implement top_landing element

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-<div class="flex justify-center"> 
-  <h1 class="text-3xl">
-    Hello World!
-  </h1>
-</div>
-`
+fetch("/src/top_landing.html").then(r=>r.text()).then((html)=>{ // get the content of products.html
+  // let element = document.createElement("html");
+  // element.innerHTML = html; // parse the html
+  // let p1 = element.querySelector("#p1");
+  document.querySelector('#app').innerHTML = html;
+});
 
-setupCounter(document.querySelector('#counter'))
+// document.querySelector('#app').innerHTML = `
+// <div class="svdc__top-landing">
+//   <div class="svdc__top-landing-left">
+//     <img src="${svdcLogo}" class="svdc__logo" />
+//     <h1></h1>
+//   </div>
+//   <div class="svdc__top-landing-right">
+
+//   </div>
+// </div>
+// `
