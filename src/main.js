2c10aa765e4e0d494d4ecfcb29fc89f945cee489
danielclamor <danielclamor@gmail.com>
2025-06-19 14:56:41 -0600

    refactor code

--- src/main.js
+++ src/main.js
@@ -1,21 +1,3 @@
-// import './style.css'
-
-fetch("/src/top_landing.html").then(r=>r.text()).then((html)=>{
+fetch("/src/main.html").then(r=>r.text()).then((html)=>{
   document.querySelector('#app').innerHTML = html;
 });
-
-fetch("/src/details_page.html").then(r=>r.text()).then((html)=>{
-  document.querySelector('#app').innerHTML += html;
-});
-
-// document.querySelector('#app').innerHTML = `
-// <div class="svdc__top-landing">
-//   <div class="svdc__top-landing-left">
-//     <img src="${svdcLogo}" class="svdc__logo" />
-//     <h1></h1>
-//   </div>
-//   <div class="svdc__top-landing-right">
-
-//   </div>
-// </div>
-// `
