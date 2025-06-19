b6089133dbe78d1d64ada9bbdbaa79047d3498e9
danielclamor <danielclamor@gmail.com>
2025-06-19 15:40:50 -0600

    add functionality to arrow button

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 fetch("/src/main.html").then(r=>r.text()).then((html)=>{
   document.querySelector('#app').innerHTML = html;
 });
+
+const arrowDownButton = document.querySelector("#button-arrow-down");
+
+arrowDownButton.addEventListener("click", function (e) {
+  const details = document.querySelector('div[name="details"]');
+
+  details.scrollIntoView({behavior: 'smooth'});
+});
