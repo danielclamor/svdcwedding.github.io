3e10ac5e4ea54bbf8611ff52739d742bc1699f75
Daniel Clamor <danielclamor@gmail.com>
2025-06-23 20:11:27 -0600

    fix file referencing

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,7 @@
-// fetch("/src/main.html").then(r=>r.text()).then((html)=>{
-//   document.querySelector('#app').innerHTML = html;
-// });
-
 const arrowDownButton = document.querySelector('#button-arrow-down');
-
-console.log(arrowDownButton);
 
 arrowDownButton.addEventListener("click", function (e) {
   const details = document.querySelector('div[name="details"]');
 
   details.scrollIntoView({behavior: 'smooth'});
-
-  console.log('scroll');
 });
